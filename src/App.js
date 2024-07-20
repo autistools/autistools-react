3f6016eff00e6fe716b56476027196f0c01900f2
Fael123Programming <rafaelfonseca1020@gmail.com>
2024-07-20 20:57:27 -0300

    Fix: navigation fixed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-// import React, { createBrowserRouter, RouterProvider } from "react-router-dom";
-import React from 'react';
-import { HashRouter, Routes, Route } from "react-router-dom";
+import React, { RouterProvider, createHashRouter } from "react-router-dom";
 import ErrorPage from "./error_page";
 import Base from "./routes/base/Base";
 import Welcome from "./routes/welcome/Welcome";
@@ -15,196 +13,95 @@ import Table from "./routes/table/Table";
 import Favorite from "./routes/favorite/Favorite";
 import objects from "./assets/objects.json";
 
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <Base />,
-//     errorElement: <ErrorPage />,
-//     children: [
-//       {
-//         path: "seja-bem-vindo",
-//         element: <Welcome />,
-//       },
-//       {
-//         path: "idade",
-//         element: <Age />,
-//       },
-//       {
-//         path: "nome",
-//         element: <Name />,
-//       },
-//       {
-//         path: "descricao",
-//         element: (
-//           <Message
-//             phrase={
-//               "Esta é uma atividade para descobrir as suas preferências, aquilo que você mais gosta"
-//             }
-//             next={"/conheca"}
-//             milli={11000}
-//           />
-//         ),
-//       },
-//       {
-//         path: "conheca",
-//         element: (
-//           <Message
-//             phrase={"Primeiro, quero que você conheça cada um dos objetos"}
-//             next={"/batman"}
-//             milli={9000}
-//           />
-//         ),
-//       },
-//       ...objects.map(({ path, file, name, next }) => {
-//         return {
-//           path: path,
-//           element: <Card file={file} name={name} next={next} />,
-//         };
-//       }),
-//       {
-//         path: "escolha",
-//         element: (
-//           <Message phrase={"Escolha um objeto preferido"} next={"/lista"} />
-//         ),
-//       },
-//       {
-//         path: "lista",
-//         element: <List />,
-//       },
-//       {
-//         path: "preparacao",
-//         element: (
-//           <Message
-//             phrase={"Agora, escolha entre dois objetos"}
-//             next={"/dois"}
-//           />
-//         ),
-//       },
-//       {
-//         path: "dois",
-//         element: <Two />,
-//       },
-//       {
-//         path: "ausencia",
-//         element: <Message phrase={"Ausência de escolha"} next={"/dois"} />,
-//       },
-//       {
-//         path: "otimo",
-//         element: <Great />,
-//       },
-//       {
-//         path: "preferido",
-//         element: <Favorite />,
-//       },
-//       {
-//         path: "tabela",
-//         element: <Table />,
-//       },
-//     ],
-//   },
-// ]);
+const router = createHashRouter([
+  {
+    path: "/",
+    element: <Base />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "seja-bem-vindo",
+        element: <Welcome />,
+      },
+      {
+        path: "idade",
+        element: <Age />,
+      },
+      {
+        path: "nome",
+        element: <Name />,
+      },
+      {
+        path: "descricao",
+        element: (
+          <Message
+            phrase={
+              "Esta é uma atividade para descobrir as suas preferências, aquilo que você mais gosta"
+            }
+            next={"/conheca"}
+            milli={11000}
+          />
+        ),
+      },
+      {
+        path: "conheca",
+        element: (
+          <Message
+            phrase={"Primeiro, quero que você conheça cada um dos objetos"}
+            next={"/batman"}
+            milli={9000}
+          />
+        ),
+      },
+      ...objects.map(({ path, file, name, next }) => {
+        return {
+          path: path,
+          element: <Card file={file} name={name} next={next} />,
+        };
+      }),
+      {
+        path: "escolha",
+        element: (
+          <Message phrase={"Escolha um objeto preferido"} next={"/lista"} />
+        ),
+      },
+      {
+        path: "lista",
+        element: <List />,
+      },
+      {
+        path: "preparacao",
+        element: (
+          <Message
+            phrase={"Agora, escolha entre dois objetos"}
+            next={"/dois"}
+          />
+        ),
+      },
+      {
+        path: "dois",
+        element: <Two />,
+      },
+      {
+        path: "ausencia",
+        element: <Message phrase={"Ausência de escolha"} next={"/dois"} />,
+      },
+      {
+        path: "otimo",
+        element: <Great />,
+      },
+      {
+        path: "preferido",
+        element: <Favorite />,
+      },
+      {
+        path: "tabela",
+        element: <Table />,
+      },
+    ],
+  },
+]);
 
-// function App() {
-//   return <RouterProvider router={router} />;
-// }
 export default function App() {
-  return (
-    <HashRouter>
-      <Routes>
-        <Route
-          path={"/"}
-          children={[
-            {
-              path: "seja-bem-vindo",
-              element: <Welcome />,
-            },
-            {
-              path: "idade",
-              element: <Age />,
-            },
-            {
-              path: "nome",
-              element: <Name />,
-            },
-            {
-              path: "descricao",
-              element: (
-                <Message
-                  phrase={
-                    "Esta é uma atividade para descobrir as suas preferências, aquilo que você mais gosta"
-                  }
-                  next={"/conheca"}
-                  milli={11000}
-                />
-              ),
-            },
-            {
-              path: "conheca",
-              element: (
-                <Message
-                  phrase={
-                    "Primeiro, quero que você conheça cada um dos objetos"
-                  }
-                  next={"/batman"}
-                  milli={9000}
-                />
-              ),
-            },
-            ...objects.map(({ path, file, name, next }) => {
-              return {
-                path: path,
-                element: <Card file={file} name={name} next={next} />,
-              };
-            }),
-            {
-              path: "escolha",
-              element: (
-                <Message
-                  phrase={"Escolha um objeto preferido"}
-                  next={"/lista"}
-                />
-              ),
-            },
-            {
-              path: "lista",
-              element: <List />,
-            },
-            {
-              path: "preparacao",
-              element: (
-                <Message
-                  phrase={"Agora, escolha entre dois objetos"}
-                  next={"/dois"}
-                />
-              ),
-            },
-            {
-              path: "dois",
-              element: <Two />,
-            },
-            {
-              path: "ausencia",
-              element: (
-                <Message phrase={"Ausência de escolha"} next={"/dois"} />
-              ),
-            },
-            {
-              path: "otimo",
-              element: <Great />,
-            },
-            {
-              path: "preferido",
-              element: <Favorite />,
-            },
-            {
-              path: "tabela",
-              element: <Table />,
-            },
-          ]}
-          element={<Base />}
-          errorElement={<ErrorPage />}
-        />
-      </Routes>
-    </HashRouter>
-  );
+  return <RouterProvider router={router} />;
 }
